d9b2cb55aaaa9c21e2c82b6a31669ca11d64263c
Michael0Jahn <ic21b029@technikum-wien.at>
2022-10-05 19:38:09 +0200

    add uptime calculations per month

---
+++ src/main/java/at/fhtw/bic/uptimefh/controller/UptimeController.java
@@ -0,0 +1,19 @@
+package at.fhtw.bic.uptimefh.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class UptimeController {
+    @GetMapping("/uptime")
+    public double calculateUptime(@RequestParam double relative){
+        double minutesofMonth = 60*24*30;
+        double defineupttimeInPercent = relative / 100;
+
+        return minutesofMonth * defineupttimeInPercent; // uptime in minutes
+    }
+
+
+}
