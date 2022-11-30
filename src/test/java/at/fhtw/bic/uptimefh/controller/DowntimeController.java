1e1957f56f2259061039b6f3a472ab756dc52c86
Michael0Jahn <ic21b029@technikum-wien.at>
2022-11-30 19:30:54 +0100

    added more Unittests

---
+++ src/test/java/at/fhtw/bic/uptimefh/controller/DowntimeController.java
@@ -0,0 +1,8 @@
+package at.fhtw.bic.uptimefh.controller;
+
+public class DowntimeController {
+
+    public double calculateDowntimeMinutes(double v){
+        return 0.0;
+    }
+}
---
+++ src/test/java/at/fhtw/bic/uptimefh/controller/DowntimeControllerTest.java
@@ -0,0 +1,46 @@
+package at.fhtw.bic.uptimefh.controller;
+
+import org.junit.jupiter.api.Test;
+
+import java.security.InvalidParameterException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class DowntimeControllerTest {
+    @Test
+    public void calculateDowntimeMinutes_whenZerothenZero() {
+        // Arrange
+        DowntimeController controller = new DowntimeController();
+
+        // Act
+        double result = controller.calculateDowntimeMinutes(0.0);
+
+        // Assert
+        assertEquals(0.0, result);
+    }
+
+    @Test
+    public void calculateDowntimeMinutes_whenNegativethenthrowZero(){
+        DowntimeController controller = new DowntimeController();
+
+
+        // Assert
+        assertThrows(InvalidParameterException.class, () -> {
+            double result = controller.calculateDowntimeMinutes(-10);
+        });
+    }
+
+    @Test
+    public void calculateDowntimeMinutes_whenGreaterthan100() {
+        // Arrange
+        DowntimeController controller = new DowntimeController();
+
+        // Assert
+        assertThrows(InvalidParameterException.class, () -> {
+            double result = controller.calculateDowntimeMinutes(110);
+        });
+    }
+
+
+}
